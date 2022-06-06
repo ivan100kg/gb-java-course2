b4f3d94f5a1b21e666f19cca86e8a3e4be9e6cee
Ivan B <ivar110kg@gmail.com>
2022-06-06 11:42:29 +0400

    Initial commit

---
+++ src/main/java/io/github/ivan100kg/MainApp.java
@@ -0,0 +1,7 @@
+package io.github.ivan100kg;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/io/github/ivan100kg/MainAppTest.java
@@ -0,0 +1,7 @@
+package io.github.ivan100kg;
+
+import junit.framework.TestCase;
+
+public class MainAppTest extends TestCase {
+
+}
