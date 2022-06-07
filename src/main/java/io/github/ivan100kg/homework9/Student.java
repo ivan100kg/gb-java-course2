6ec76cb3c9186f2428b630e6100a07c6e7d887d3
Ivan B <ivar110kg@gmail.com>
2022-06-07 10:35:07 +0400

    Add Student.java

---
+++ src/main/java/io/github/ivan100kg/homework9/Student.java
@@ -0,0 +1,14 @@
+package io.github.ivan100kg.homework9;
+
+@Table(title = "students")
+public class Student {
+    @Column
+    private String name;
+    @Column
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
