3d882d336eb51804554ebb21ec07e8c79687a075
Ivan B <ivar110kg@gmail.com>
2022-06-07 10:34:49 +0400

    Add Annotations

---
+++ src/main/java/io/github/ivan100kg/homework9/Column.java
@@ -0,0 +1,12 @@
+package io.github.ivan100kg.homework9;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Column {
+
+}
