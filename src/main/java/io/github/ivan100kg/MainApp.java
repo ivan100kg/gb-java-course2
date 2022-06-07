c467fe7f5b75646915685e9a4083fa9229a95f52
Ivan B <ivar110kg@gmail.com>
2022-06-07 09:05:36 +0400

    Update MainApp.java

--- src/main/java/io/github/ivan100kg/MainApp.java
+++ src/main/java/io/github/ivan100kg/MainApp.java
@@ -6,10 +6,25 @@ public class MainApp {
     private static Connection connection;
     private static Statement statement;
     private static PreparedStatement preparedStatement;
+
     public static void main(String[] args) {
         try {
             connect();
-            insertEx();
+//            insertEx();
+            long time = System.currentTimeMillis();
+//            connection.setAutoCommit(false);
+            for (int i = 0; i < 1000; i++) {
+                preparedStatement.setString(1, "Bob" + i);
+                preparedStatement.setInt(2, 50 + i % 10);
+//                preparedStatement.executeUpdate();
+//                preparedStatement.addBatch();
+            }
+            Savepoint sp1 = connection.setSavepoint();
+            connection.rollback(sp1);
+//            connection.commit();
+//            preparedStatement.executeBatch();
+            System.out.println(System.currentTimeMillis() - time);
+
             selectEx();
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -19,7 +34,7 @@ public class MainApp {
     }
 
     public static void selectEx() throws SQLException {
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM students;");
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM students ORDER BY id DESC LIMIT 5;");
         while (resultSet.next()) {
             System.out.println(resultSet.getInt(1) + " " +
                     resultSet.getString("name") + " " +
@@ -37,6 +52,8 @@ public class MainApp {
             Class.forName("org.sqlite.JDBC");
             connection = DriverManager.getConnection("jdbc:sqlite:main.db");
             statement = connection.createStatement();
+            preparedStatement = connection.prepareStatement(
+                    "INSERT INTO students (name, score) VALUES (?, ?);");
         } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
