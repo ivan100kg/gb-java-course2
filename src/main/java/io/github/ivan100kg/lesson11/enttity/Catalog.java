6fdab91ed406e1febcd22d9e9b5dae0ad2dc07e3
Ivan B <ivar110kg@gmail.com>
2022-06-17 19:46:54 +0400

    Add Catalog.java

---
+++ src/main/java/io/github/ivan100kg/lesson11/enttity/Catalog.java
@@ -0,0 +1,45 @@
+package io.github.ivan100kg.lesson11.enttity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Catalog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Catalog() {
+    }
+
+    public Catalog(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Catalog{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
