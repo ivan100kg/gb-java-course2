1442d17ada2ced98873f8b41f5ec9fa855972680
Ivan B <ivar110kg@gmail.com>
2022-06-20 21:07:20 +0400

    Add Item.java

---
+++ src/main/java/io/github/ivan100kg/homework12/Item.java
@@ -0,0 +1,34 @@
+package io.github.ivan100kg.homework12;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "items")
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Long id;
+
+    @Column(name = "val")
+    private int val;
+
+    public Item() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+}
