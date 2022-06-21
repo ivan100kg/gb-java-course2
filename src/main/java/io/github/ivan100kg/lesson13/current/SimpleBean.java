d2ab30418d3d75b7092d8d11149c094dc019828e
Ivan B <ivar110kg@gmail.com>
2022-06-21 18:54:25 +0400

    Add file SimpleBean.java

---
+++ src/main/java/io/github/ivan100kg/lesson13/current/SimpleBean.java
@@ -0,0 +1,17 @@
+package io.github.ivan100kg.lesson13.current;
+
+public class SimpleBean {
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void doSomething() {
+        System.out.println("Simple Bean print = " + value);
+    }
+}
---
+++ src/main/java/io/github/ivan100kg/lesson13/examples/SimpleBean.java
@@ -0,0 +1,13 @@
+package io.github.ivan100kg.lesson13.examples;
+
+public class SimpleBean {
+    private String data;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
