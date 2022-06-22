362ce58c3af5de86a233083040142b938eee44c6
Ivan B <ivar110kg@gmail.com>
2022-06-22 17:12:20 +0400

    Add file Cart.java

---
+++ src/main/java/io/github/ivan100kg/homework13/Cart.java
@@ -0,0 +1,28 @@
+package io.github.ivan100kg.homework13;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class Cart {
+    private List<Product> products;
+
+    public Cart() {
+        System.out.println("Cart is created");
+    }
+
+    @Autowired
+    public void addProduct(Product product) {
+        if (products == null) {
+            products = new ArrayList<>();
+        }
+        products.add(product);
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+}
